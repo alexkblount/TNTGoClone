2941e3b57fce3074539fe15169a0604b7142f5c7
Alex Blount <alexkblount@outlook.com>
2021-07-11 14:03:17 -0700

    updating Droid for Maui

--- TNTGoClone.Android/MainActivity.cs
+++ TNTGoClone.Android/MainActivity.cs
@@ -6,29 +6,25 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Maui;
 
 namespace TNTGoClone.Droid
 {
 	[Activity(Label = "TNTGoClone", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	public class MainActivity : MauiAppCompatActivity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
-			TabLayoutResource = Resource.Layout.Tabbar;
-			ToolbarResource = Resource.Layout.Toolbar;
+			// this.TabLayoutResource = Resource.Layout.Tabbar;
+			// this.ToolbarResource = Resource.Layout.Toolbar;
 
 			base.OnCreate(savedInstanceState);
-
-			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-			FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
-			LoadApplication(new App());
 		}
-		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-		{
-			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		// public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		// {
+		// 	// Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-		}
+		// 	base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		// }
 	}
 }
---
+++ TNTGoClone.Android/MainApplication.cs
@@ -0,0 +1,18 @@
+using System;
+using Android.App;
+using Android.Runtime;
+using Microsoft.Maui;
+using TNTGoClone;
+
+namespace TNTGoClone.Droid
+{
+
+	[Application]
+	public class MainApplication : MauiApplication<Startup>
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+			: base(handle, ownership)
+		{
+		}
+	}
+}
