4b21eff45002977dbde7c80a585e92416e0da0e0
Alex Blount <alexkblount@outlook.com>
2021-07-12 12:55:09 -0700

    adding Single Project

---
+++ SingleProject/TNTGClone/TNTGClone/Platforms/Android/MainApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Microsoft.Maui;
+using TNTGoClone;
+
+namespace TNTGoClone.Droid
+{
+	[Application]
+	public class MainApplication : MauiApplication<Startup>
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+			: base(handle, ownership)
+		{
+		}
+	}
+}
---
+++ SingleProject/TNTGClone/TNTGClone/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using TNTGoClone;
+
+namespace TNTGoClone.Mac
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
